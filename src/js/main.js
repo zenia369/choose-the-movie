b7056e1d75cbc962cb2dfd35526fe184590822e5
Yev//:k <58997885+zenia369@users.noreply.github.com>
2025-10-19 13:33:25 +0300

    Update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -52,53 +52,6 @@ filterMod.addEventListener('click', activeFilterMod)
 standartMod.addEventListener('click', activeStandartMod);
 btnFiltreLoadMore.addEventListener('click', activeBtnForFiltreLoadMore)
 
-/* Пагинація */
-
-// window.addEventListener('scroll', throttle(checkPosition));
-// window.addEventListener("resize", throttle(checkPosition));
-
-// async function checkPosition() {
-//     // Нам потребуется знать высоту документа и высоту экрана.
-//     const height = document.body.offsetHeight
-//     const screenHeight = window.innerHeight
-  
-//     // Они могут отличаться: если на странице много контента,
-//     // высота документа будет больше высоты экрана (отсюда и скролл).
-  
-//     // Записываем, сколько пикселей пользователь уже проскроллил.
-//     const scrolled = window.scrollY
-  
-//     // Обозначим порог, по приближении к которому
-//     // будем вызывать какое-то действие.
-//     // В нашем случае — четверть экрана до конца страницы.
-//     const threshold = height - screenHeight / 4
-  
-//     // Отслеживаем, где находится низ экрана относительно страницы.
-//     const position = scrolled + screenHeight
-  
-//     if (position >= threshold) {
-//       // Если мы пересекли полосу-порог, вызываем нужное действие.
-//        await loadCard();
-//     }
-// }
-
-// function throttle(callee, timeout = 3000) {
-//     let timer = null
-  
-//     return function perform(...args) {
-//       if (timer) return
-  
-//       timer = setTimeout(() => {
-//         callee(...args)
-  
-//         clearTimeout(timer)
-//         timer = null
-//       }, timeout)
-//     }
-// }
-  
-/* КІНЕЦЬ Пагинація */
-
 
 function activeStandartMod(e) {
     if(!e.path[1].classList.contains('active')) {
